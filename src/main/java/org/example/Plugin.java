69003c874d6b1a45553e0d00dae066e53d54f856
ConstantineQAQ <357208746@qq.com>
2022-09-09 16:30:49 +0800

    修改了定时发消息以及在群聊中主动发消息的回复

--- src/main/java/org/example/Plugin.java
+++ src/main/java/org/example/Plugin.java
@@ -1,12 +1,20 @@
 package org.example;
 
+import net.mamoe.mirai.Bot;
 import net.mamoe.mirai.console.plugin.jvm.JavaPlugin;
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescriptionBuilder;
 import net.mamoe.mirai.event.GlobalEventChannel;
 import org.example.entil.BotSend;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public final class Plugin extends JavaPlugin {
+    Timer timer = new Timer();
+    BotSend botSend = new BotSend();
     public static final Plugin INSTANCE = new Plugin();
+
+
 
     private Plugin() {
         super(new JvmPluginDescriptionBuilder("org.example.plugin", "1.0-SNAPSHOT")
@@ -19,5 +27,12 @@ public final class Plugin extends JavaPlugin {
     public void onEnable() {
         getLogger().info("兔兔的课表插件启动了捏!");
         GlobalEventChannel.INSTANCE.registerListenerHost(new BotSend());
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                botSend.getClassDetilNegative();
+                this.cancel();
+            }
+        },3000);
     }
 }
--- src/main/java/org/example/entil/BotSend.java
+++ src/main/java/org/example/entil/BotSend.java
@@ -1,63 +1,103 @@
 package org.example.entil;
 
 import kotlinx.coroutines.CoroutineScope;
+import net.mamoe.mirai.Bot;
+import net.mamoe.mirai.contact.Friend;
 import net.mamoe.mirai.event.*;
 import net.mamoe.mirai.event.events.FriendEvent;
 import net.mamoe.mirai.event.events.FriendMessageEvent;
 import net.mamoe.mirai.event.events.GroupEvent;
 import net.mamoe.mirai.event.events.GroupMessageEvent;
 import org.example.util.DateOfDay;
+import org.example.util.DateUtil;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Date;
+import java.util.Objects;
+import java.util.Timer;
 
 
 public class BotSend extends SimpleListenerHost {
+    Timer timer = new Timer();
     Date date = new Date();
     DateOfDay dateOfDay = new DateOfDay();
     DataBaseGet dataBaseGet = new DataBaseGet();
 
+
+
     @EventHandler
-    public ListeningStatus ClassFriendMessage(FriendMessageEvent event) throws Exception {
-        getClassDetilNegativeP(event);
+    public ListeningStatus ClassFriendMessageP(FriendMessageEvent event) throws Exception {
         getClassDetilPosstive(event);
 
         return ListeningStatus.LISTENING;
     }
 
     @EventHandler
-    public ListeningStatus ClassGroupMessage(GroupMessageEvent event){
+    public ListeningStatus ClassGroupMessage(GroupMessageEvent event) throws Exception {
 
         getClassDetilNegativeG(event);
         return ListeningStatus.LISTENING;
     }
 
-    //每天早上八点主动给我发送消息
-    public void getClassDetilNegativeP(FriendEvent event) {
-        if (dateOfDay.getTime(date).equals("13:40")) {
+    public void getClassDetilNegative(){
+        int nowHour = DateUtil.getNowHour();
+        if(nowHour == 8) {
+            Long qqId = null;
+            Long GroupId = null;
             try {
-                event.getBot().getFriend(357208746).sendMessage(dataBaseGet.getDayClass());
-                event.getBot().getFriend(357208746).sendMessage("----------------------" +
+                qqId = 357208746L;
+                GroupId = 167493031L;
+                Bot.getInstances().get(0).getFriend(qqId).sendMessage(dataBaseGet.getDayClass());
+                Bot.getInstances().get(0).getFriend(qqId).sendMessage("----------------------" +
+                        "\n" + "这周是本学期第" + dateOfDay.getWeekNumber(date) + "周" +
+                        "\n" + "----------------------" +
+                        "\n" + dateOfDay.getWeek(date) + " " + dateOfDay.getTime(date));
+                Bot.getInstances().get(0).getFriend(GroupId).sendMessage(dataBaseGet.getDayClass());
+                Bot.getInstances().get(0).getFriend(GroupId).sendMessage("----------------------" +
                         "\n" + "这周是本学期第" + dateOfDay.getWeekNumber(date) + "周" +
                         "\n" + "----------------------" +
                         "\n" + dateOfDay.getWeek(date) + " " + dateOfDay.getTime(date));
             } catch (Exception e) {
-                event.getBot().getFriend(357208746).sendMessage(e.getMessage());
+                Bot.getInstances().get(0).getFriend(qqId).sendMessage(e.getMessage());
             }
         }
     }
 
-    //每天早上八点主动给宿舍群发消息
-    public void getClassDetilNegativeG(GroupEvent event) {
-        if (dateOfDay.getTime(date) == "08:00") {
-            try {
-                event.getBot().getGroup(167493031).sendMessage(dataBaseGet.getDayClass());
-                event.getBot().getGroup(167493031).sendMessage("----------------------" +
-                        "\n" + "这周是本学期第" + dateOfDay.getWeekNumber(date) + "周" +
-                        "\n" + "----------------------" +
-                        "\n" + dateOfDay.getWeek(date) + " " + dateOfDay.getTime(date));
-            } catch (Exception e) {
-                event.getBot().getFriend(357208746).sendMessage(e.getMessage());
+/*    //每天早上八点主动给我发送消息
+    public void getClassDetilNegativeP(FriendEvent event) {
+        int nowHour = DateUtil.getNowHour();
+        if (nowHour == 14) {
+            int nowMinute = DateUtil.getNowMinute();
+            if(nowMinute == 56){
+                try {
+                    event.getBot().getFriend(357208746).sendMessage(dataBaseGet.getDayClass());
+                    event.getBot().getFriend(357208746).sendMessage("----------------------" +
+                            "\n" + "这周是本学期第" + dateOfDay.getWeekNumber(date) + "周" +
+                            "\n" + "----------------------" +
+                            "\n" + dateOfDay.getWeek(date) + " " + dateOfDay.getTime(date));
+                } catch (Exception e) {
+                    event.getBot().getFriend(357208746).sendMessage(e.getMessage());
+                }
             }
+        }
+    }*/
+
+    //每天早上八点主动给宿舍群发消息
+    public void getClassDetilNegativeG(GroupMessageEvent event) throws Exception {
+        //获取DateofDay对象以调用get方法
+        DateOfDay dateOfDay = new DateOfDay();
+        //获取Date对象
+        Date date = new Date();
+        //获取DataBaseGet对象，以便于调用获取课表的方法
+        DataBaseGet dataBaseGet = new DataBaseGet();
+        String s = event.getMessage().contentToString();
+        if (s.matches("^(?:(?:课表)|(?:今日课表))[\\s]*$"))
+        {
+            event.getGroup().sendMessage(dataBaseGet.getDayClass());
+            event.getGroup().sendMessage("----------------------" +
+                    "\n" + "这周是本学期第" + dateOfDay.getWeekNumber(date) + "周" +
+                    "\n" + "----------------------" +
+                    "\n" + dateOfDay.getWeek(date) + " " + dateOfDay.getTime(date));
         }
     }
 
