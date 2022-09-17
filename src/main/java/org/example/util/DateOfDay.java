4da691afac13935860cbbdb663caf678ddbef451
ConstantineQAQ <357208746@qq.com>
2022-09-17 16:58:11 +0800

    更改注释

--- src/main/java/org/example/util/DateOfDay.java
+++ src/main/java/org/example/util/DateOfDay.java
@@ -8,26 +8,26 @@ import java.util.GregorianCalendar;
 public class DateOfDay {
 
 
-    //获取当前是星期几
+    //获取当前是星期几。
     public static String getWeek(Date date) {
         SimpleDateFormat sdf = new SimpleDateFormat("EEEE");
         return sdf.format(date);
     }
 
-    //获取现在是几点几分
+    //获取现在是几点几分。
     public static String getTime(Date date) {
         SimpleDateFormat sdf2 = new SimpleDateFormat("HH:mm");
         return sdf2.format(date);
     }
 
-    //获取现在是第几周
+    //获取现在是第几周。
     public static int getWeekNumber(Date date) {
         GregorianCalendar g = new GregorianCalendar();
         g.setTime(date);
         return g.get(Calendar.WEEK_OF_YEAR) - 35;
     }
 
-    //获取明天是星期几
+    //获取明天是星期几。
     public static String getWeek_tomorrow(){
         SimpleDateFormat sdf = new SimpleDateFormat("EEEE");
         Calendar calendar = new GregorianCalendar();
@@ -36,7 +36,7 @@ public class DateOfDay {
         return sdf.format(calendar.getTime());
     }
 
-    //获取明天是第几周
+    //获取明天是第几周。
     public static int getWeekNumber_tomorrow(){
         GregorianCalendar g = new GregorianCalendar();
         Calendar calendar = new GregorianCalendar();
